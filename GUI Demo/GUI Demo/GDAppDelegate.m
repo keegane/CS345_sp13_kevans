174ba8eeec2f16c2c152fa95866ade1dd635d697
Guest User <Guest@Mac110-Selesnya.local>
2013-01-23 10:19:50 -0500

    Sending in Notes of hello, goodbye.

---
+++ GUI Demo/GUI Demo/GDAppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  GDAppDelegate.m
+//  GUI Demo
+//
+//  Created by Guest User on 1/23/13.
+//  Copyright (c) 2013 Guest User. All rights reserved.
+//
+
+#import "GDAppDelegate.h"
+
+@implementation GDAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+- (IBAction)do_Hello:(id)sender {
+    NSString* currentText = nil;
+    currentText = [[self theTextField] stringValue];
+    currentText = [NSString stringWithFormat:@"Hello, %@", currentText];
+    [[self theTextField] setStringValue:currentText];
+}
+
+- (IBAction)do_Goodbye:(id)sender {
+    NSString* currentText = nil;
+    currentText = [[self theTextField] stringValue];
+    currentText = [NSString stringWithFormat:@"Goodbye, %@", currentText];
+    [[self theTextField] setStringValue:currentText];
+
+}
+@end
