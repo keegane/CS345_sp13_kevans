c18590b1dc062c89f4a38dc2c4d47cbab91f8650
Guest User <Guest@Mac110-Selesnya.local>
2013-01-31 14:21:14 -0500

    Test 4 repeated. Test 4 refactored.

--- TDD Waltz/TDD WaltzTests/Example.m
+++ TDD Waltz/TDD WaltzTests/Example.m
@@ -4,6 +4,8 @@
 
 #import "Example.h"
 
+static NSString *const currentReminderIdKey = @"currentReminderId";
+
 @implementation Example
 {
     NSUserDefaults *_userDefaults;
@@ -15,17 +17,21 @@
     if (self)
         _userDefaults = userDefaults;
     return self;
-    
+}
+
+- (NSNumber *)determineNextReminderIdFromUserDefaults
+{
+    NSNumber *reminderId = [_userDefaults objectForKey: currentReminderIdKey];
+    if (reminderId)
+        return @([reminderId integerValue] + 1);
+    else
+        return @0;
 }
 
 - (NSNumber *)nextReminderId
 {
-    NSNumber *reminderId = [_userDefaults objectForKey: @"currentReminderId"];
-    if (reminderId)
-        reminderId = @([reminderId integerValue] + 1);
-    else
-        reminderId = @0;
-    [_userDefaults setObject:reminderId forKey:@"currentReminderId"];
+    NSNumber *reminderId = [self determineNextReminderIdFromUserDefaults];
+    [_userDefaults setObject:reminderId forKey:currentReminderIdKey];
     return reminderId;
 }
 
